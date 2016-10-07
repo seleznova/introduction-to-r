ac327b49bcd0485c2a67e5a2b13ec2a75007281d
seleznova <mary.kuzmenko@gmail.com>
2016-10-07 22:48:02 +0300

    Add file basics.r

---
+++ basics.r
@@ -0,0 +1,37 @@
+### Exercise 1
+
+# Calculate 3 + 4
+3 + 4
+
+# Calculate 6 + 12
+6 + 12
+
+
+### Exercise 2
+# An addition
+5 + 5
+
+# A subtraction
+5 - 5
+
+# A multiplication
+3 * 5
+
+# A division
+(5 + 5) / 2
+
+# Exponentiation
+2^5
+
+# Modulo
+28 %% 6
+
+### Exercise 3
+
+# Assign the value 42 to x
+x <- 42
+
+# Print out the value of the variable x
+x
+
+### Exercise 4
