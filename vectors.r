b0339f57cdb09a36d4d56df8a0d857e1b9616ec7
seleznova <mary.kuzmenko@gmail.com>
2016-10-07 23:12:14 +0300

    Update in file vectors.r

--- vectors.r
+++ vectors.r
@@ -5,12 +5,33 @@ vegas <- "Go!"
 
 ### Exercise 2
 
+numeric_vector <- c(1, 10, 49)
+character_vector <- c("a", "b", "c")
+
+# Complete the code for boolean_vector
+boolean_vector <- c(TRUE, FALSE, TRUE)
 
 ### Exercise 3
 
+# Poker winnings from Monday to Friday
+poker_vector <- c(140, -50, 20, -120, 240)
+
+# Roulette winnings from Monday to Friday
+roulette_vector <- c(-24, -50, 100, -350, 10)
 
 ### Exercise 4
 
+# Poker winnings from Monday to Friday
+poker_vector <- c(140, -50, 20, -120, 240)
+
+# Roulette winnings from Monday to Friday
+roulette_vector <- c(-24, -50, 100, -350, 10)
+
+# Assign days as names of poker_vector
+names(poker_vector) <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+
+# Assign days as names of roulette_vectors
+names(roulette_vector) <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
 
 ### Exercise 5
 
