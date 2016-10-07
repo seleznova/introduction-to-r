7ac8368585f8940009b907cd00929f2e17607a79
seleznova <mary.kuzmenko@gmail.com>
2016-10-07 22:59:02 +0300

    Update file basics.r

--- basics.r
+++ basics.r
@@ -35,3 +35,60 @@ x <- 42
 x
 
 ### Exercise 4
+# Assign the value 5 to the variable my_apples
+my_apples <- 5
+
+# Print out the value of the variable my_apples
+my_apples
+
+### Exercise 5
+
+# Assign a value to the variables my_apples and my_oranges
+my_apples <- 5
+my_oranges <- 6
+
+# Add these two variables together
+my_apples + my_oranges
+
+# Create the variable my_fruit
+my_fruit <- my_apples + my_oranges
+
+### Exercise 6
+
+# Assign a value to the variable my_apples
+my_apples <- 5
+
+# Fix the assignment of my_oranges
+my_oranges <- 6
+
+# Create the variable my_fruit and print it out
+my_fruit <- my_apples + my_oranges
+my_fruit
+
+### Exercise 7
+
+# Change my_numeric to be 42
+my_numeric <- 42
+
+# Change my_character to be "universe"
+my_character <- "universe"
+
+# Change my_logical to be FALSE
+my_logical <- FALSE
+
+### Exercise 8
+
+# Declare variables of different types
+my_numeric <- 42
+my_character <- "universe"
+my_logical <- FALSE
+
+# Check class of my_numeric
+class(my_numeric)
+
+# Check class of my_character
+class(my_character)
+
+# Check class of my_logical
+class(my_logical)
+### Exercise 9
