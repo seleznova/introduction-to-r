f00ce60ad3be1f52c282fc3511619132b8403efb
seleznova <mary.kuzmenko@gmail.com>
2016-10-30 07:24:16 +0200

    ColSums

--- matrix.r
+++ matrix.r
@@ -64,3 +64,14 @@ star_wars_matrix2
 
 # Combine both Star Wars trilogies in one matrix
 all_wars_matrix <- rbind(star_wars_matrix, star_wars_matrix2)
+
+# ColSums
+
+# all_wars_matrix is available in your workspace
+all_wars_matrix
+
+# Total revenue for US and non-US
+total_revenue_vector <- colSums(all_wars_matrix)
+
+# Print out total_revenue_vector
+total_revenue_vector
