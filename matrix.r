4d137ca9ee6e080fbcaccfe2ce8f1c0567e7067e
seleznova <mary.kuzmenko@gmail.com>
2016-10-30 07:22:16 +0200

    Adding a row

--- matrix.r
+++ matrix.r
@@ -56,3 +56,11 @@ worldwide_vector <- rowSums(star_wars_matrix)
 
 # Bind the new variable worldwide_vector as a column to star_wars_matrix
 all_wars_matrix <- cbind(star_wars_matrix, worldwide_vector)
+
+# Adding a row
+# star_wars_matrix and star_wars_matrix2 are available in your workspace
+star_wars_matrix
+star_wars_matrix2
+
+# Combine both Star Wars trilogies in one matrix
+all_wars_matrix <- rbind(star_wars_matrix, star_wars_matrix2)
