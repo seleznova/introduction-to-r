054460dfb3252e6da6d432e905a5b8d2232f0c41
seleznova <mary.kuzmenko@gmail.com>
2016-10-30 07:28:09 +0200

    Selection of matrix element

--- matrix.r
+++ matrix.r
@@ -75,3 +75,20 @@ total_revenue_vector <- colSums(all_wars_matrix)
 
 # Print out total_revenue_vector
 total_revenue_vector
+
+# Selection of matrices
+
+# all_wars_matrix is available in your workspace
+all_wars_matrix
+
+# Select the non-US revenue for all movies
+non_us_all <- all_wars_matrix[,2]
+
+# Average non-US revenue
+mean(non_us_all)
+
+# Select the non-US revenue for first two movies
+non_us_some <- all_wars_matrix[1:2,2]
+
+# Average non-US revenue for first two movies
+mean(non_us_some)
