fab133525804cf8500b04578d9e04cd8bac904fe
seleznova <mary.kuzmenko@gmail.com>
2016-10-30 07:33:50 +0200

    Arithmetic with matrices improved

--- matrix.r
+++ matrix.r
@@ -102,3 +102,18 @@ visitors <- all_wars_matrix/5
 
 # Print the estimate to the console
 visitors
+
+# Arithmetic with matrices improved
+
+# all_wars_matrix and ticket_prices_matrix are available in your workspace
+all_wars_matrix
+ticket_prices_matrix
+
+# Estimated number of visitors
+visitors <- all_wars_matrix/ticket_prices_matrix
+
+# US visitors
+us_visitors <- visitors[,1]
+
+# Average number of US visitors
+mean(us_visitors)
